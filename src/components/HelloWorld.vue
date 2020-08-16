91bc702ce2b6d230589835df55ba5c02d340d143
Geanina <masgras.geanina@hotmail.com>
2020-08-16 17:41:43 +0200

    add alert on click button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-    </p>
+    <button  v-on:click="onClickMe()">Click me</button>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+  
+    onClickMe: function() {
+      alert('This is your first  button in Vue.js')
+    }
   }
 }
-</script>
 
+
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
